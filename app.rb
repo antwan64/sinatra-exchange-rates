f86b8e294f2bee83e44869d66013b21521574620
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-02 20:32:38 +0000

    Still working on it

--- app.rb
+++ app.rb
@@ -1,13 +1,70 @@
+# /app.rb
+
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
-api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_API_KEY"]}"
-
+# define a route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  # build the API url, including the API key in the query string
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data.fetch("currencies").keys
+
+  # render a view template where I show the symbols
+   erb(:homepage)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data.fetch("currencies").keys
+
+  erb(:conversion)
+  
+  # some more code to parse the URL and render a view template
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  @raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(@raw_data_string)
+
   
 
+
+  erb(:cur_to_cur)
+  
+  # some more code to parse the URL and render a view template
 end
